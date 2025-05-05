f2523dd2282e769e3f0ccbdc110ab0986797ed61
kss418 <jis1595@naver.com>
2025-05-05 22:51:45 +0900

    쓰레드 풀 파일 추가

---
+++ server/include/Thread_Pool.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include <boost/asio.hpp>
+
+class Thread_Pool{
+public:
+    Thread_Pool(int thread_count);
+private:
+    
+};
+
---
+++ server/src/Thread_Pool.cpp
@@ -0,0 +1,5 @@
+#include "../include/Thread_Pool.hpp"
+
+Thread_Pool::Thread_Pool(int thread_count){
+
+};
