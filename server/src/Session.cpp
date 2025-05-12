b59562f3f5037c91b071f4f4de6b23398ab729da
kss418 <jis1595@naver.com>
2025-05-12 15:04:55 +0900

    self 람다에 캡쳐

--- server/src/Session.cpp
+++ server/src/Session.cpp
@@ -7,10 +7,9 @@ Session::Session(int32_t id, std::shared_ptr<boost::asio::ip::tcp::socket> socke
 }
 
 void Session::Read(){
-    auto self = shared_from_this();
     socket->async_read_some(
         boost::asio::buffer(buf),
-        [self](const boost::system::error_code& ec, std::size_t bytes_transffered){
+        [self = shared_from_this()](const boost::system::error_code& ec, std::size_t bytes_transffered){
             self->Handle_Read(ec, bytes_transffered);
         }
     );
@@ -21,11 +20,10 @@ void Session::Set_Handle_Close(std::function<void(std::shared_ptr<Session>)> f){
 }
 
 void Session::Write(const std::string& msg){
-    auto self = shared_from_this();
     boost::asio::async_write(
         *socket,
         boost::asio::buffer(msg),
-        [self](const boost::system::error_code& ec, std::size_t bytes_transffered){
+        [self = shared_from_this()](const boost::system::error_code& ec, std::size_t bytes_transffered){
             self->Handle_Write(ec, bytes_transffered);
         }
     );
